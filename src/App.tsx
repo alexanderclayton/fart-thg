acb3e15e7881edcc2ef08c0b1c8cc315a281eb8a
Alex Clayton <alexander.clayton.email@gmail.com>
2023-08-20 11:05:53 -0500

    basic header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 //import//
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Home } from "./pages/Home"
+import { Header } from "./components/Header"
 
 function App() {
 
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
