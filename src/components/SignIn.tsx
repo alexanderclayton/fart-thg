2945f83588416ce13070a1aae845bb6cbd663ae2
Alex Clayton <alexander.clayton.email@gmail.com>
2023-08-22 20:31:53 -0500

    basic signin modal styling

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { auth } from "../firebase/firebaseAuth";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { FirebaseError } from "firebase/app";
+import { AiOutlineUser, AiOutlineLock } from "react-icons/ai";
 
 export const SignIn: React.FC = () => {
   const [modal, setModal] = useState<boolean>(false);
@@ -24,10 +25,10 @@ export const SignIn: React.FC = () => {
 
   const signIn = async (): Promise<void> => {
     try {
-        await signInWithEmailAndPassword(auth, email, password)
-        console.log("Signed In!")
-        setEmail("")
-        setPassword("")
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log("Signed In!");
+      setEmail("");
+      setPassword("");
     } catch (error: unknown) {
       if (error instanceof FirebaseError) {
         if (error.code === "auth/wrong-password") {
@@ -44,23 +45,47 @@ export const SignIn: React.FC = () => {
     }
   };
 
+  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    signIn();
+  };
+
   return (
-    <div>
+    <div className="flex flex-col items-end">
       <button onClick={handleModal}>Sign In</button>
-      <div>
-        <input
-          type="text"
-          placeholder="Email..."
-          onChange={handleEmail}
-          value={email}
-        />
-        <input
-          type="text"
-          placeholder="Password..."
-          onChange={handlePassword}
-          value={password}
-        />
-        <button onClick={signIn}>Sign In</button>
+      <div className={modal ? "fixed w-[420px] bg-blue-200 top-20 right-10" : "hidden"}>
+        <form
+          onSubmit={handleSubmit}
+          className="w-full flex flex-col items-center p-2"
+        >
+          <h1 className="text-3xl font-bold">Sign In</h1>
+          <div className="w-full border rounded-full p-2 flex justify-between m-2">
+            <input
+              type="text"
+              placeholder="Email..."
+              onChange={handleEmail}
+              value={email}
+              className="bg-transparent outline-none px-4 w-full"
+            />
+            <AiOutlineUser size={30} />
+          </div>
+          <div className="w-full border rounded-full p-2 flex justify-between m-2">
+            <input
+              type="password"
+              placeholder="Password..."
+              onChange={handlePassword}
+              value={password}
+              className="bg-transparent outline-none px-4 w-full"
+            />
+            <AiOutlineLock size={30} />
+          </div>
+          <button
+            type="submit"
+            className="w-full rounded-full bg-white py-2 m-2 font-bold"
+          >
+            Sign In
+          </button>
+        </form>
       </div>
     </div>
   );
