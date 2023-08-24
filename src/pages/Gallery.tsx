4cee31047b9947636e1d1364554052d51373a337
Alex Clayton <alexander.clayton.email@gmail.com>
2023-08-24 12:43:08 -0500

    basic gallery before/after toggle template

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,7 +1,11 @@
 //import//
 
+import { House4416 } from "../components/House4416"
+
 export const Gallery: React.FC = () => {
   return (
-    <div>Gallery</div>
+    <div>
+        <House4416 />
+    </div>
   )
 }
